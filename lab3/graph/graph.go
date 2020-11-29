cf19e085f3b1f2051d997928b549b0667c197774
JakubGogola <jakubgogola97@gmail.com>
2020-11-29 10:56:24 +0100

    update

--- lab3/graph/graph.go
+++ lab3/graph/graph.go
@@ -7,14 +7,19 @@ package graph
 
 // AddVertex creates a new vertex with given coords and adds it to graph.
 func (g *Graph) AddVertex(x, y float64) (v Vertex) {
-	v = Vertex{x, y}
+	v = Vertex{X: x, Y: y}
 	g.Vertices = append(g.Vertices, v)
 	return v
 }
 
 // AddEdge create a new edge between given vertices and adds it to graph.
 func (g *Graph) AddEdge(v, w Vertex) (e Edge) {
-	e = Edge{v, w}
+	e = Edge{V: v, W: w}
 	g.Edges = append(g.Edges, e)
 	return e
+}
+
+// DeleteEdge deletes edge between given vertices
+func (g *Graph) DeleteEdge(v, w Vertex) {
+
 }
