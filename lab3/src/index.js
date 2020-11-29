f952a471848acddc3894dff79e84dae96d2e0c49
JakubGogola <jakubgogola97@gmail.com>
2020-11-29 13:13:24 +0100

    lab3: recursive algorithm

--- lab3/src/index.js
+++ lab3/src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello, world!');
+import { DelaunayRecursive } from './algorithms/DelaunayRecursive.js';
+
+const recursive = DelaunayRecursive.create(
+    [[1.1, 2.3],
+    [4.5, 2.3],
+    [10.1, 12.3]]
+);
+
+const result = recursive.triangulate();
+
+console.log(result);
