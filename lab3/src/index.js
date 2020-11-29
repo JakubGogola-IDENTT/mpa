944a370c0af1b6cf2310e85f89db4e0578231a18
JakubGogola <j.gogola@livechatinc.com>
2020-11-29 20:07:07 +0100

    lab3: merged

--- lab3/src/index.js
+++ lab3/src/index.js
@@ -1,8 +1,6 @@
 /* eslint-disable no-console */
 import { DelaunayRecursive } from './algorithms/recursive/DelaunayRecursive.js';
 import { DelaunayIterative } from './algorithms/iterative/DelaunayIterative.js';
-import { Vertex } from './algorithms/iterative/Vertex.js';
-import { generatePoints } from './analytics/generatePoints.js';
 import { testing } from './analytics/testing.js';
 
-testing(DelaunayRecursive);
+testing(DelaunayIterative);
